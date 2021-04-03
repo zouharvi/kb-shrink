fix mrr computation

src/misc/utils.py
@@ -1,7 +1,7 @@
 import pickle
 import json
 import numpy as np
-import scann
+from scipy.spatial.distance import minkowski
 import torch
 
 DEVICE = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
@@ -39,15 +39,18 @@ def load_dataset(path, keep="inputs"):
         return [parse_dataset_line(line, keep) for line in f.readlines()]
 
 def vec_sim(data, sim_func=np.inner):
+    """
+    Compute vector similarity matrix, excluding the diagonal.
+    """
     return [
-        [sim_func(vec1, vec2) for vec2 in data]
-        for vec1 in data
+        [sim_func(vec1, vec2) for i2, vec2 in enumerate(data) if i1 != i2]
+        for i1, vec1 in enumerate(data)
     ]
 
 def vec_sim_order(data, sim_func=np.inner):
     return [
         sorted(
-            range(len(data)),
+            range(len(sims)),
             key=lambda x: sims[x],
             reverse=True
         ) for sims in vec_sim(data, sim_func)
@@ -64,3 +67,6 @@ def mrr(order_old, order_new, n, report=False):
         print(f"MRR (top {n}) is {mrr_val:.3f} (best is 1, worst is 0)")
 
     return mrr_val
+
+def l2_sim(x, y):
+    return -minkowski(x, y)

src/reduce_dim/autoencoder.py
@@ -2,16 +2,17 @@
 
 import sys
 sys.path.append("src")
+from misc.utils import l2_sim
 
 from misc.utils import mrr, read_keys_pickle, save_keys_pickle, DEVICE, vec_sim_order
 import argparse
-from scipy.spatial.distance import minkowski
+import numpy as np
 import torch
 import torch.nn as nn
 
 
 class Autoencoder(nn.Module):
-    def __init__(self, model, batchSize=128, learningRate=0.001):
+    def __init__(self, model, bottleneck, batchSize=128, learningRate=0.001):
         super().__init__()
 
         if model == 1:
@@ -21,11 +22,11 @@ class Autoencoder(nn.Module):
                 nn.ReLU(True),
                 nn.Linear(512, 256),
                 nn.ReLU(True),
-                nn.Linear(256, 256)
+                nn.Linear(256, bottleneck)
             )
             # Decoder Network
             self.decoder = nn.Sequential(
-                nn.Linear(256, 256),
+                nn.Linear(bottleneck, 256),
                 nn.ReLU(True),
                 nn.Linear(256, 512),
                 nn.ReLU(True),
@@ -61,7 +62,8 @@ class Autoencoder(nn.Module):
         self.dataLoader = torch.utils.data.DataLoader(
             dataset=data, batch_size=self.batchSize, shuffle=True
         )
-        order_old = vec_sim_order(data.cpu())
+        order_old_ip = vec_sim_order(data.cpu(), sim_func=np.inner)
+        order_old_l2 = vec_sim_order(data.cpu(), sim_func=l2_sim)
 
         for epoch in range(epochs):
             self.train(True)
@@ -82,10 +84,10 @@ class Autoencoder(nn.Module):
                 
                 # V^2 similarity computations is computationally expensive, skip if not necessary
                 if False:
-                    order_new = vec_sim_order(encoded)
-                    mrr_val = mrr(order_old, order_new, 20, report=False)
-                    order_new = vec_sim_order(encoded, lambda x,y: -minkowski(x,y))
-                    mrr_val_l2 = mrr(order_old, order_new, 20, report=False)
+                    order_new = vec_sim_order(encoded, sim_func=np.inner)
+                    mrr_val = mrr(order_old_ip, order_new, 20, report=False)
+                    order_new = vec_sim_order(encoded, sim_func=l2_sim)
+                    mrr_val_l2 = mrr(order_old_l2, order_new, 20, report=False)
                     print(f'epoch [{epoch+1}/{epochs}], loss_l2: {loss.data:.7f}, mrr_ip: {mrr_val:.3f}, mrr_l2: {mrr_val_l2:.3f}')
                 elif False:
                     order_new = vec_sim_order(encoded)
@@ -107,6 +109,9 @@ if __name__ == '__main__':
         '--model', default=1, type=int,
         help='Which model to use (1 - big)')
     parser.add_argument(
+        '--bottleneck', default=256, type=int,
+        help='Dimension of the bottleneck layer')
+    parser.add_argument(
         '--epochs', default=1000, type=int)
     parser.add_argument(
         '--seed', type=int, default=0)
@@ -114,7 +119,7 @@ if __name__ == '__main__':
     torch.manual_seed(args.seed)
     data = read_keys_pickle(args.keys_in)
     data = torch.Tensor(data).to(DEVICE)
-    model = Autoencoder(args.model)
+    model = Autoencoder(args.model, args.bottleneck)
     model.trainModel(data, args.epochs)
     model.train(False)
     with torch.no_grad():

src/reduce_dim/mrr_test.py
@@ -3,7 +3,8 @@
 import sys
 sys.path.append("src")
 
-from misc.utils import read_keys_pickle, mrr, vec_sim_order
+from misc.utils import read_keys_pickle, mrr, vec_sim_order, l2_sim
+import numpy as np
 import argparse
 
 if __name__ == '__main__':
@@ -22,7 +23,12 @@ if __name__ == '__main__':
     data_old = read_keys_pickle(args.keys_in_old)
     data_new = read_keys_pickle(args.keys_in_new)
 
-    order_old = vec_sim_order(data_old)
-    order_new = vec_sim_order(data_new)
+    order_old_ip = vec_sim_order(data_old, sim_func=np.inner)
+    order_old_l2 = vec_sim_order(data_old, sim_func=l2_sim)
+    order_new_ip = vec_sim_order(data_new, sim_func=np.inner)
+    order_new_l2 = vec_sim_order(data_new, sim_func=l2_sim)
+    mrr_val_ip = mrr(order_old_ip, order_new_ip, 20, report=False)
+    mrr_val_l2 = mrr(order_old_l2, order_new_l2, 20, report=False)
 
-    mrr_val = mrr(order_old, order_new, args.top_n, report=True)
+    print(mrr_val_ip)
+    print(mrr_val_l2)

src/reduce_dim/principal_components.py
@@ -2,12 +2,11 @@
 
 import sys, os
 sys.path.append("src")
-from misc.utils import mrr, read_keys_pickle, vec_sim_order
+from misc.utils import mrr, read_keys_pickle, vec_sim_order, l2_sim
 import argparse
 from sklearn.decomposition import PCA
 from scipy.spatial.distance import minkowski
 import numpy as np
-import matplotlib.pyplot as plt
 from pympler.asizeof import asizeof
 
 parser = argparse.ArgumentParser(description='Explore vector distribution')
@@ -18,19 +17,22 @@ args = parser.parse_args()
 data = read_keys_pickle(args.keys_in)
 origSize = asizeof(data)
 
-order_old = vec_sim_order(data)
+order_old_l2 = vec_sim_order(data, sim_func=l2_sim)
+order_old_ip = vec_sim_order(data, sim_func=np.inner)
 
-
-print(f"{'Method':<30}   {'Size':>4}            {'Loss':>8}   {'MRR 20':>8}")
+print(f"{'Method':<21} {'Size':<6} {'L2 Loss':<7} {'IPMRR':<0} {'L2MRR':<0}")
 
 def summary_performance(name, dataReduced, dataReconstructed):
-    order_new = vec_sim_order(dataReduced)
-    mrr_val = mrr(order_old, order_new, 20, report=False)
+    order_new_ip = vec_sim_order(dataReduced, sim_func=np.inner)
+    order_new_l2 = vec_sim_order(dataReduced, sim_func=l2_sim)
+    mrr_val_ip = mrr(order_old_ip, order_new_ip, 20, report=False)
+    mrr_val_l2 = mrr(order_old_l2, order_new_l2, 20, report=False)
     size = asizeof(dataReduced)
     distances = []
     for vec, vecNew in zip(data, dataReconstructed):
         distances.append(minkowski(vec, vecNew, 2))
-    print(f"{name:<30} {size/1024/1024:>4.1f}MB ({size/origSize:.1f}x)   {np.average(distances):>10.7f}    {mrr_val:>5.3f}")
+    name = name.replace("float", "f")
+    print(f"{name:<21} {size/origSize:>5.3f}x {np.average(distances):>7.5f} {mrr_val_ip:>5.3f} {mrr_val_l2:>5.3f}")
 
 def pca_performance(components):
     model = PCA(n_components=components).fit(data)
@@ -39,24 +41,27 @@ def pca_performance(components):
 
 def precision_performance(newType):
     dataReduced = data.astype(newType)
-    summary_performance(f"Precision ({newType})", dataReduced, dataReduced.astype("float32"))
+    summary_performance(f"Prec ({newType})", dataReduced, dataReduced.astype("float32"))
 
 def precision_pca_performance(components, newType):
     dataReduced = data.astype(newType)
     model = PCA(n_components=components).fit(dataReduced)
     dataReduced = model.transform(dataReduced).astype("float32")
-    summary_performance(f"Precision ({newType}), PCA ({components})", dataReduced, model.inverse_transform(dataReduced).astype("float32"))
+    summary_performance(f"Prec ({newType}), PCA ({components})", dataReduced, model.inverse_transform(dataReduced).astype("float32"))
 
 def pca_precision_preformance(components, newType):
     model = PCA(n_components=components).fit(data)
     dataReduced = model.transform(data)
     dataReduced = dataReduced.astype(newType)
-    summary_performance(f"PCA ({components}), Precision ({newType})", dataReduced, model.inverse_transform(dataReduced.astype("float32")))
+    summary_performance(f"PCA ({components}), Prec ({newType})", dataReduced, model.inverse_transform(dataReduced.astype("float32")))
 
 summary_performance(f"Original ({data.dtype})", data, data)
-pca_performance(512)
+pca_performance(32)
+pca_performance(64)
+pca_performance(128)
 pca_performance(256)
 precision_performance("float16")
-precision_pca_performance(512, "float16")
-pca_precision_preformance(512, "float16")
-pca_precision_preformance(256, "float16")
+pca_precision_preformance(32, "float16")
+pca_precision_preformance(64, "float16")
+pca_precision_preformance(128, "float16")
+precision_pca_performance(256, "float16")