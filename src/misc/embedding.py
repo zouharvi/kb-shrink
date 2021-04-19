enable GPU embedding computation (fix unknown), resolve #5

src/misc/embedding.py
@@ -6,24 +6,27 @@ from misc.utils import parse_dataset_line, DEVICE
 import pickle
 import argparse
 from transformers import BertTokenizer, BertModel
+import torch
 
 class BertWrap():
     def __init__(self, embd_model="bert-base-cased"):
         self.tokenizer = BertTokenizer.from_pretrained(embd_model)
         self.model = BertModel.from_pretrained(embd_model, return_dict=True, output_hidden_states=True)
         self.model.train(False)
+        self.model.to(DEVICE)
 
     def sentence_embd(self, sentence, type_out):
         encoded_input = self.tokenizer(sentence, return_tensors='pt')
-        encoded_input = encoded_input
-        output = self.model(**encoded_input)
+        encoded_input = encoded_input.to(DEVICE)
+        with torch.no_grad():
+            output = self.model(**encoded_input)
         if type_out == "pooler":
             # each dimension is bounded [0, 1] 
-            return output["pooler_output"][0].detach().numpy()
+            return output["pooler_output"][0].cpu().numpy()
         elif type_out == "tokens_avg":
             # select the last layer
             # dimensions are *not* bounded [0, 1]
-            return output["hidden_states"][-1][0].mean(dim=0).detach().numpy()
+            return output["hidden_states"][-1][0].mean(dim=0).cpu().numpy()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -37,7 +40,7 @@ if __name__ == "__main__":
     with open(args.dataset, "r") as fread, open(args.embd_out, "wb") as fwrite:
         pickler = pickle.Pickler(fwrite)
         for i, line in enumerate(fread):
-            line = parse_dataset_line(line)
+            line = parse_dataset_line(line, keep="inputs")
             output = bert.sentence_embd(line["input"], args.type_out)
 
             if i % 10 == 0: