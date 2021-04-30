aa1f70d86ca40bae0dfe1d1c45c14414b71b5b68
Vilém Zouhar <vilem.zouhar@gmail.com>
2021-04-30 19:16:14 +0200

    add isompa

--- src/reduce_dim/manifold.py
+++ src/reduce_dim/manifold.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append("src")
 from misc.utils import read_keys_pickle, mrr_ip_fast, mrr_l2_fast
 import argparse
-from sklearn.manifold import TSNE, Isomap
+from sklearn.manifold import TSNE, Isomap, MDS
 
 parser = argparse.ArgumentParser(description='Visualization of embeddings')
 parser.add_argument(
@@ -12,9 +12,15 @@ parser.add_argument(
 args = parser.parse_args()
 
 data = read_keys_pickle(args.keys)[:5000]
-model = TSNE(n_components=3, random_state=0)
-# model = Isomap(n_components=64)
+# model = TSNE(n_components=3, random_state=0)
+# model = MDS(n_components=64)
+
+# isomap
+model = Isomap(n_components=64)
 data_new = model.fit_transform(data)
+data_new = data_new.copy() # is not C-continuous
+
+
 
 mrr_l2_fast(data, data_new, report=True)
 mrr_ip_fast(data, data_new, report=True)
