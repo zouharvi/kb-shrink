177eb174395864d28cd4fad0b08a594699ae94e1
Vilém Zouhar <vilem.zouhar@gmail.com>
2021-05-01 23:49:38 +0200

    add mds

--- src/reduce_dim/manifold.py
+++ src/reduce_dim/manifold.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append("src")
 from misc.utils import read_keys_pickle, mrr_ip_fast, mrr_l2_fast
 import argparse
-from sklearn.manifold import TSNE, Isomap, MDS
+from sklearn.manifold import TSNE, Isomap, MDS, SpectralEmbedding
 
 parser = argparse.ArgumentParser(description='Visualization of embeddings')
 parser.add_argument(
@@ -13,14 +13,20 @@ args = parser.parse_args()
 
 data = read_keys_pickle(args.keys)[:5000]
 # model = TSNE(n_components=3, random_state=0)
-# model = MDS(n_components=64)
+
+# mds
+model = MDS(n_components=64)
+data_new = model.fit_transform(data)
 
 # isomap
-model = Isomap(n_components=64)
-data_new = model.fit_transform(data)
-data_new = data_new.copy() # is not C-continuous
+# model = Isomap(n_components=64)
+# data_new = model.fit_transform(data)
+# data_new = data_new.copy() # is not C-continuous
 
-
+# spectral embedding 
+# model = SpectralEmbedding(n_components=64)
+# data_new = model.fit_transform(data)
+# data_new = data_new.copy() # is not C-continuous
 
 mrr_l2_fast(data, data_new, report=True)
 mrr_ip_fast(data, data_new, report=True)
