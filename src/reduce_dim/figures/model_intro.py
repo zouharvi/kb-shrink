42f17e4e46abbd04ef3af44027755f7ca886604b
Vilém Zouhar <vilem.zouhar@gmail.com>
2021-06-05 22:05:46 +0200

    updated model intro values

--- src/reduce_dim/figures/model_intro.py
+++ src/reduce_dim/figures/model_intro.py
@@ -5,46 +5,6 @@ sys.path.append("src")
 import numpy as np
 import matplotlib.pyplot as plt
 
-# data outdated
-DATA_ACC = {
-    "DPR\n(avg)": {
-        "ip_fast": 0.532,
-        "ip": 0.8176,
-        "l2_fast": 0.5508,
-        "l2": 0.7154,
-    },
-    "Sentence\nBert\n(avg)": {
-        "ip_fast": 0.6268,
-        "ip": 0.7284,
-        "l2_fast": 0.5976,
-        "l2": 0.7304,
-    },
-    "Bert\n(avg)": {
-        "ip_fast": 0.5270,
-        "ip": 0.5420,
-        "l2_fast": 0.5256,
-        "l2": 0.6142,
-    },
-    "DPR\n(cls)": {
-        "ip_fast": 0.461,
-        "ip": 0.8862,
-        "l2_fast": 0.4682,
-        "l2": 0.6036,
-    },
-    "Sentence\nBert\n(cls)": {
-        "ip_fast": 0.5738,
-        "ip": 0.7178,
-        "l2_fast": 0.555,
-        "l2": 0.7036,
-    },
-    "Bert\n(cls)": {
-        "ip_fast": 0.1834,
-        "ip": 0.0322,
-        "l2_fast": 0.1428,
-        "l2": 0.1894,
-    },
-}
-
 DATA_RPREC = {
     "DPR\n(Avg)": {
         "ip_fast": 0.3159,
@@ -84,42 +44,97 @@ DATA_RPREC = {
     },
 }
 
-# DATA = DATA_ACC
-DATA = DATA_RPREC
+DATA_RPREC_NORM = {
+    "DPR\n(Avg)": {
+        "ip_fast": 0.3171,
+        "ip": 0.3280,
+        "l2_fast": 0.3194,
+        "l2": 0.3280,
+    },
+    "Sentence\nBERT\n(Avg)": {
+        "ip_fast": 0.3028,
+        "ip": 0.3097,
+        "l2_fast": 0.3013,
+        "l2": 0.3097,
+    },
+    "BERT\n(Avg)": {
+        "ip_fast": 0.2024,
+        "ip": 0.2124,
+        "l2_fast": 0.2022,
+        "l2": 0.2124,
+    },
+    "DPR\n[CLS]": {
+        "ip_fast": 0.3229,
+        "ip": 0.3398,
+        "l2_fast": 0.3206,
+        "l2": 0.3398,
+    },
+    "Sentence\nBERT\n[CLS]": {
+        "ip_fast": 0.2648,
+        "ip": 0.2732,
+        "l2_fast": 0.2637,
+        "l2": 0.2732,
+    },
+    "BERT\n[CLS]": {
+        "ip_fast": 0.0318,
+        "ip": 0.0347,
+        "l2_fast": 0.0315,
+        "l2": 0.0347,
+    },
+}
 
-POS = np.arange(len(DATA))
+POS = np.arange(len(DATA_RPREC))
 BARHEIGHT = 0.2
-YERRMOCK = np.random.rand(len(DATA))/10000
+YERRMOCK = np.random.rand(len(DATA_RPREC))/10000
 
 plt.figure(figsize=(4.4, 4))
 ax = plt.gca()
 ax.bar(
     POS + 0.2 * 0,
-    [x["ip"] for x in DATA.values()],
-    width=BARHEIGHT, label="IP", yerr=YERRMOCK,
+    [x["ip"] for x in DATA_RPREC.values()],
+    width=BARHEIGHT, label="IP",
     color="tab:blue", hatch="",
 )
+ax.scatter(
+    POS + 0.2 * 0,
+    [x["ip"] for x in DATA_RPREC_NORM.values()],
+    color="black", marker="_"
+).set_zorder(10)
 ax.bar(
     POS + 0.2 * 1,
-    [x["ip_fast"] for x in DATA.values()],
-    width=BARHEIGHT, label="IP fast", yerr=YERRMOCK,
+    [x["ip_fast"] for x in DATA_RPREC.values()],
+    width=BARHEIGHT, label="IP fast",
     color="lightskyblue", hatch="...",
 )
+ax.scatter(
+    POS + 0.2 * 1,
+    [x["ip_fast"] for x in DATA_RPREC_NORM.values()],
+    color="black", marker="_"
+).set_zorder(10)
 ax.bar(
     POS + 0.2 * 2,
-    [x["l2"] for x in DATA.values()],
-    width=BARHEIGHT, label="L2", yerr=YERRMOCK,
+    [x["l2"] for x in DATA_RPREC.values()],
+    width=BARHEIGHT, label="L2",
     color="tab:red", hatch="",
 )
+ax.scatter(
+    POS + 0.2 * 2,
+    [x["l2"] for x in DATA_RPREC_NORM.values()],
+    color="black", marker="_"
+).set_zorder(10)
 ax.bar(
     POS + 0.2 * 3,
-    [x["l2_fast"] for x in DATA.values()],
-    width=BARHEIGHT, label="L2 fast", yerr=YERRMOCK,
+    [x["l2_fast"] for x in DATA_RPREC.values()],
+    width=BARHEIGHT, label="L2 fast",
     color="lightcoral", hatch="...",
 )
+ax.scatter(
+    POS + 0.2 * 3,
+    [x["l2_fast"] for x in DATA_RPREC_NORM.values()],
+    color="black", marker="_"
+).set_zorder(10)
 ax.set_xticks(POS + 0.15 * 2)
-ax.set_xticklabels(DATA.keys())
-# ax.set_ylabel("Accuracy (n=20)")
+ax.set_xticklabels(DATA_RPREC.keys())
 ax.set_ylabel("R-Precision")
 plt.legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left", mode="expand", ncol=4)
 plt.tight_layout()
