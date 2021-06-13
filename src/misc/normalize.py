fix normalization, batch retrieval speedup

src/misc/normalize.py
@@ -2,21 +2,31 @@
 
 import sys; sys.path.append("src")
 import argparse
-from misc.utils import read_pickle, save_pickle
+from misc.utils import read_pickle, save_pickle, norm_data, center_data
 import numpy as np
 
 parser = argparse.ArgumentParser(description='Normalize embeddings')
 parser.add_argument('--data-in', default="data/eli5-dev.embd")
 parser.add_argument('--data-out', default="data/eli5-dev-norm.embd")
+parser.add_argument('--center', action="store_true")
+parser.add_argument('--norm', action="store_true")
 args = parser.parse_args()
+
+assert args.center or args.norm
 
 print("Loading")
 data = read_pickle(args.data_in)
-print("First query element[:4]", data["queries"][0][:4])
-print("Normalizing queries")
-data["queries"] = data["queries"] / np.linalg.norm(data["queries"], axis=1)[:, np.newaxis]
-print("Normalizing docs")
-data["docs"] = data["docs"] / np.linalg.norm(data["docs"], axis=1)[:, np.newaxis]
-print("First query element[:4]", data["queries"][0][:4])
+print("First query element[:4]", data["queries"][0][:4], "Norm:", np.linalg.norm(data["queries"][0]))
+
+if args.center:
+    print("Centering")
+    data = center_data(data)
+    
+if args.center:
+    print("Normalizing")
+    data = norm_data(data)
+
+print("First query element[:4]", data["queries"][0][:4], "Norm:", np.linalg.norm(data["queries"][0]))
+
 print("Saving")
 save_pickle(args.data_out, data)

src/misc/utils.py
@@ -56,8 +56,8 @@ def center_data(data):
 def norm_data(data):
     data["docs"] = np.array(data["docs"])
     data["queries"] = np.array(data["queries"])
-    data["docs"] = data["docs"] / np.linalg.norm(data["docs"])
-    data["queries"] = data["queries"] / np.linalg.norm(data["queries"])
+    data["docs"] = data["docs"] / np.linalg.norm(data["docs"], axis=1)[:, np.newaxis]
+    data["queries"] = data["queries"] / np.linalg.norm(data["queries"], axis=1)[:, np.newaxis]
     return data
 
 def l2_sim(x, y):
@@ -96,10 +96,16 @@ def order_l2(data_queries, data_docs, n, fast):
         index.train(data_docs)
     index.add(data_docs)
 
+    BATCH_LIMIT = 256
     def n_new_gen():
+        batch = []
         for i, d in enumerate(data_queries):
-            out = index.search(np.array([d]), n)[1][0]
-            yield out
+            batch.append(d)
+            if len(batch) >= BATCH_LIMIT or i == len(data_queries)-1:
+                out = index.search(np.array(batch), n)[1]
+                for el in out:
+                    yield el
+                batch = []
 
     # pass generators so that the resulting vectors don't have to be stored in memory
     return n_new_gen()
@@ -122,7 +128,7 @@ def order_ip(data_queries, data_docs, n, fast):
         index.train(data_docs)
     index.add(data_docs)
 
-    BATCH_LIMIT = 16
+    BATCH_LIMIT = 256
     def n_new_gen():
         batch = []
         for i, d in enumerate(data_queries):