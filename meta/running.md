17d1f0f68a68cef39fcb4bcc96db949770771995
Vilém Zouhar <vilem.zouhar@gmail.com>
2021-10-01 18:31:21 +0200

    make zscores more memory efficient

--- meta/running.md
+++ meta/running.md
@@ -1,9 +1,9 @@
-date|action
--|-
-28 sep|sbert-c full embd restarted
-18 sep|embd for the rest of the models
-30 sep|run dpr-c uncompressed --all
-30 sep|run dpr-c zscores
+date|action|status
+-|-|-
+28 sep|sbert-c full embd restarted|running
+18 sep|embd for the rest of the models|partially finished
+1 oct|run dpr-c uncompressed `--all`|fail,restart
+1 oct|run dpr-c zscores (sequentially with `--std` and `--std --center`|fail,restart
 ||
 finished|
 4 sep| full embd for dpr-c
--- src/reduce_dim/manifold_learning/zscores.py
+++ src/reduce_dim/manifold_learning/zscores.py
@@ -8,33 +8,27 @@ import argparse
 from sklearn import preprocessing
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data', default="/data/kilt-hp/dpr-c-5000.embd_cn")
+parser.add_argument('--data', default="/data/big-hp/dpr-c.embd")
 parser.add_argument('--center', action="store_true")
 parser.add_argument('--std', action="store_true")
-parser.add_argument('--seed', type=int, default=0)
 args = parser.parse_args()
 data = read_pickle(args.data)
 
 print("Preparing model")
-model_d = preprocessing.StandardScaler(
+data["docs"] = preprocessing.StandardScaler(
     with_std=args.std, with_mean=args.center
-).fit(data["docs"])
-model_q = preprocessing.StandardScaler(
+).fit_transform(data["docs"])
+data["queries"] = preprocessing.StandardScaler(
     with_std=args.std, with_mean=args.center
-).fit(data["queries"])
+).fit_transform(data["queries"])
 
-dataNew = {
-    "docs": model_d.transform(data["docs"]),
-    "queries": model_q.transform(data["queries"]),
-}
 print(data["docs"][0][:5])
-print(dataNew["docs"][0][:5])
 
 val_ip_pca = rprec_a_ip(
-    dataNew["queries"], dataNew["docs"], data["relevancy"], data["relevancy_articles"], data["docs_articles"], fast=True
+    data["queries"], data["docs"], data["relevancy"], data["relevancy_articles"], data["docs_articles"], fast=True
 )
 val_l2_pca = rprec_a_l2(
-    dataNew["queries"], dataNew["docs"], data["relevancy"], data["relevancy_articles"], data["docs_articles"], fast=True
+    data["queries"], data["docs"], data["relevancy"], data["relevancy_articles"], data["docs_articles"], fast=True
 )
 print(f"ip: {val_ip_pca:.4f}, l2: {val_l2_pca:.4f} (zscores)")
 
