709338856d47ffe8fc9d28d11a849520f7cf99a5
Vilém Zouhar <vilem.zouhar@gmail.com>
2021-12-18 13:32:00 +0100

    ready irrelevant effects

--- src/reduce_dim/pca/run_irrelevant.py
+++ src/reduce_dim/pca/run_irrelevant.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 import random
-import sys; sys.path.append("src")
+import sys
+sys.path.append("src")
 from misc.load_utils import read_pickle, center_data, norm_data, sub_data
 from misc.retrieval_utils import rprec_a_l2, rprec_a_ip
-import numpy as np
-import torch
 import argparse
 from sklearn.decomposition import PCA
 
@@ -19,7 +18,8 @@ args = parser.parse_args()
 data = read_pickle(args.data)
 data_big = read_pickle(args.data_big)
 
-def summary_performance(name, dataReduced):
+
+def summary_performance(dataReduced):
     if args.post_cn:
         dataReduced = center_data(dataReduced)
         dataReduced = norm_data(dataReduced)
@@ -46,23 +46,21 @@ def summary_performance(name, dataReduced):
     return val_ip, val_l2
 
 
+def safe_transform(model, array):
+    return [model.transform([x])[0] for x in array]
+
+
 def pca_performance_d(components, data, data_train):
     model = PCA(
         n_components=components,
         random_state=args.seed
     ).fit(data_train["docs"])
     dataReduced = {
-        "queries": model.transform(data["queries"]),
-        "docs": model.transform(data["docs"])
-    }
-    dataReconstructed = {
-        "queries": model.inverse_transform(dataReduced["queries"]),
-        "docs": model.inverse_transform(dataReduced["docs"])
+        "queries": safe_transform(model, data["queries"]),
+        "docs": safe_transform(model, data["docs"])
     }
     return summary_performance(
-        f"PCA-D ({components})",
         dataReduced,
-        dataReconstructed
     )
 
 
@@ -72,48 +70,54 @@ def pca_performance_q(components, data, data_train):
         random_state=args.seed
     ).fit(data_train["queries"])
     dataReduced = {
-        "queries": model.transform(data["queries"]),
-        "docs": model.transform(data["docs"])
+        "queries": safe_transform(model, data["queries"]),
+        "docs": safe_transform(model, data["docs"])
     }
     return summary_performance(
-        f"PCA-Q ({components})",
         dataReduced
     )
+
 
 data_train = dict(data)
 data = sub_data(data, train=False, in_place=True)
 data_train = sub_data(data_train, train=True, in_place=True)
-# data_big = sub_data(data_big, train=True, in_place=True)
 
 print(len(data["docs"]), len(data["queries"]))
 print(len(data_train["docs"]), len(data_train["queries"]))
 print(len(data_big["docs"]), len(data_big["queries"]))
 
 logdata = []
-
-for num_samples in [10**3, (10**3)*5, (10**4)*5, 10**5, (10**5)*5, 10**6, (10**6)*5, 10**7, (10**7)*5]:
-
-    new_data = dict(data)
+for num_samples in [10**3, (10**3) * 5, (10**4) * 5, 10**5, (10**5) * 5, 10**6, len(data_train["docs"]), (10**6) * 5, 10**7, (10**7) * 5]:
+    new_data = dict(data_train)
     if num_samples < len(new_data["docs"]):
         new_data["docs"] = random.sample(new_data["docs"], num_samples)
     else:
-        new_data["docs"] += random.sample(data_big["docs"], num_samples-len(new_data["docs"]))
+        new_data["docs"] += random.sample(
+            data_big["docs"],
+            num_samples - len(new_data["docs"])
+        )
 
-    val_ip, val_l2 = pca_performance_d(args.dim, data, data_train)
+    val_ip, val_l2 = pca_performance_d(args.dim, data, new_data)
 
     logdata.append({
         "val_ip": val_ip, "val_l2": val_l2,
-        "type": "q", 
+        "num_samples": num_samples,
+        "type": "train_data",
     })
+
+    if num_samples > len(data_train["docs"]):
+        new_data = dict(data)
+        new_data["docs"] += random.sample(
+            data_big["docs"],
+            num_samples - len(data["docs"])
+        )
+        val_ip, val_l2 = pca_performance_d(args.dim, new_data, data_train)
+        logdata.append({
+            "val_ip": val_ip, "val_l2": val_l2,
+            "num_samples": num_samples,
+            "type": "eval_data",
+        })
 
     # continuously override the file
     with open(args.logfile, "w") as f:
         f.write(str(logdata))
-
-
-# val_ip, val_l2, loss_q, loss_d = pca_performance_d(args.dim)
-# print({
-#     "val_ip": val_ip, "val_l2": val_l2,
-#     "loss_q": loss_q, "loss_d": loss_d,
-#     "type": "d"
-# })
