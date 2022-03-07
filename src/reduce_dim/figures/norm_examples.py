708c4b682ba28f405be5b2f64d024f123804415b
Vilém Zouhar <vilem.zouhar@gmail.com>
2022-03-07 22:18:10 +0100

    final touches to speed graph

--- src/reduce_dim/figures/norm_examples.py
+++ src/reduce_dim/figures/norm_examples.py
@@ -16,6 +16,7 @@ N_VECTORS = 15
 INDEX_0 = 2
 INDEX_1 = 3
 
+
 def center(data):
     return data - data.mean(axis=0)
 
@@ -25,8 +26,10 @@ def norm(data):
 
 
 # data = np.random.RandomState(3).random(N_VECTORS*3).reshape(N_VECTORS, 3)
-data_q = np.array(read_pickle("computed/dpr-c-pruned-500.embd")["queries"])[:,[INDEX_0,INDEX_1]]
-data_d = np.array(read_pickle("computed/dpr-c-pruned-500.embd")["docs"])[:,[INDEX_0,INDEX_1]]
+data_q = np.array(read_pickle("computed/dpr-c-pruned-500.embd")
+                  ["queries"])[:, [INDEX_0, INDEX_1]]
+data_d = np.array(read_pickle("computed/dpr-c-pruned-500.embd")
+                  ["docs"])[:, [INDEX_0, INDEX_1]]
 
 data_q_c = center(data_q)[:N_VECTORS]
 data_q_cn = norm(center(data_q))[:N_VECTORS] * 1
@@ -99,14 +102,18 @@ h_d_cn = plt.scatter(
 #     **STYLE, **STYLE_D,
 # )
 
-extra = Rectangle((0, 0), 1, 1, fc="w", fill=False,
-                  edgecolor='none', linewidth=0)
+extra = Rectangle(
+    (0, 0), 1, 1, fc="w",
+    fill=False, edgecolor='none', linewidth=0
+)
 
 plt.legend(
     [extra, h_q, h_q_c, h_q_cn, extra, h_d, h_d_c, h_d_cn],
     [
-        "$\\textbf{Queries}$", h_q.get_label(), h_q_c.get_label(), h_q_cn.get_label(),
-        "$\\textbf{Docs}$", h_d.get_label(), h_d_c.get_label(), h_d_cn.get_label()
+        "$\\textbf{Queries}$", h_q.get_label(
+        ), h_q_c.get_label(), h_q_cn.get_label(),
+        "$\\textbf{Docs}$", h_d.get_label(
+        ), h_d_c.get_label(), h_d_cn.get_label()
     ],
     bbox_to_anchor=(1, 1), loc="upper left"
 )
--- src/reduce_dim/figures/speed.py
+++ src/reduce_dim/figures/speed.py
@@ -1,69 +1,168 @@
 #!/usr/bin/env python3
 
+"""
+./src/reduce_dim/figures/speed.py \
+    --logfile-pca-scikit computed/speed/speed_pca_scikit_reddrapes_* \
+    --logfile-pca-torch computed/speed/speed_pca_torch_cpu_reddrapes_* \
+    --logfile-pca-torch-gpu computed/speed/speed_pca_torch_gpu_reddrapes_* \
+    --logfile-auto-gpu computed/speed/speed_auto_torch_gpu_reddrapes_* \
+    --logfile-auto computed/speed/speed_auto_torch_cpu_reddrapes_* 
+"""
+
 import sys
 sys.path.append("src")
 import misc.plot_utils
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import scipy.stats as st
+import numpy as np
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--logfile-pca-scikit', default="computed/pca_speed_puffyemery.log")
-parser.add_argument('--logfile-pca-torch', default="computed/pca_speed_puffyemery_torch.log")
-parser.add_argument('--logfile-pca-torch-gpu', default="computed/pca_speed_puffyemery_torch_gpu.log")
-parser.add_argument('--logfile-auto-gpu', default="computed/auto_speed_puffyemery_gpu.log")
-parser.add_argument('--logfile-auto', default="computed/auto_speed_puffyemery.log")
+parser.add_argument('--logfile-pca-scikit', default=["computed/pca_speed_puffyemery.log"], nargs="+")
+parser.add_argument('--logfile-pca-torch', default=["computed/pca_speed_puffyemery_torch.log"], nargs="+")
+parser.add_argument('--logfile-pca-torch-gpu', default=["computed/pca_speed_puffyemery_torch_gpu.log"], nargs="+")
+parser.add_argument('--logfile-auto-gpu', default=["computed/auto_speed_puffyemery_gpu.log"], nargs="+")
+parser.add_argument('--logfile-auto', default=["computed/auto_speed_puffyemery.log"], nargs="+")
 parser.add_argument('--key', default=0, type=int)
 args = parser.parse_args()
 
-with open(args.logfile_pca_scikit, "r") as f:
-    data_pca_scikit = eval(f.read())
-with open(args.logfile_pca_torch, "r") as f:
-    data_pca_torch = eval(f.read())
-with open(args.logfile_pca_torch_gpu, "r") as f:
-    data_pca_torch_gpu = eval(f.read())
-with open(args.logfile_auto_gpu, "r") as f:
-    data_auto_gpu = eval(f.read())
-with open(args.logfile_auto, "r") as f:
-    data_auto = eval(f.read())
+def deep_merge(data_orig, data_new):
+    """
+    inefficient deep merge but works
+    """
+    if data_orig is None:
+        data_orig = [{k: [v] for k, v in x.items()}
+                        for x in data_new]
+    else:
+        # a complicated deep merge
+        for x in data_new:
+            data_orig = [
+                # deep merge
+                {
+                    k: v + [x[k]]
+                    for k, v
+                    in xo.items()
+                }
+                # find the correct line
+                if x["type"] == xo["type"][0] and x["dim"] == xo["dim"][0]
+                else xo
+                for xo in data_orig
+            ]
+    return data_orig
 
-DIMS = sorted(list(set([x["dim"] for x in data_pca_scikit])), key=lambda x: int(x))
+data_pca_scikit = None
+data_pca_torch = None
+data_pca_torch_gpu = None
+data_auto_gpu = None
+data_auto = None
+for file in args.logfile_pca_scikit:
+    with open(file, "r") as f:
+        data_pca_scikit = deep_merge(data_pca_scikit, eval(f.read()))
+for file in args.logfile_pca_torch:
+    with open(file, "r") as f:
+        data_pca_torch = deep_merge(data_pca_torch, eval(f.read()))
+for file in args.logfile_pca_torch_gpu:
+    with open(file, "r") as f:
+        data_pca_torch_gpu = deep_merge(data_pca_torch_gpu, eval(f.read()))
+for file in args.logfile_auto_gpu:
+    with open(file, "r") as f:
+        data_auto_gpu = deep_merge(data_auto_gpu, eval(f.read()))
+for file in args.logfile_auto:
+    with open(file, "r") as f:
+        data_auto = deep_merge(data_auto, eval(f.read()))
+
+DIMS = sorted(list(set([x["dim"][0] for x in data_pca_scikit])), key=lambda x: int(x))
 DISPLAY_DIMS = [32, 256, 512, 768]
 
-plt.figure(figsize=(7, 4))
-ax = plt.gca()
+plt.figure(figsize=(7, 5))
+ax1 = plt.gca()
+extra = Rectangle(
+    (0, 0), 1, 1, fc="w",
+    fill=False, edgecolor='none', linewidth=0
+)
 
-plt.rcParams["lines.linewidth"] = 2.2
+# plt.rcParams["lines.linewidth"] = 2.2
 
-ax.plot(DIMS, [x["encode_time"] for x in data_pca_scikit], label="PCA (scikit), encode", color="tab:green", linestyle="-", alpha=0.75)
-ax.plot(DIMS, [x["encode_time"] for x in data_pca_torch_gpu], label="PCA (Torch, GPU), encode", color="tab:blue", linestyle="-", alpha=0.75)
-ax.plot(DIMS, [x["encode_time"] for x in data_pca_torch], label="PCA (Torch, CPU), encode", color="tab:cyan", linestyle="-", alpha=0.75)
-ax.plot(DIMS, [x["encode_time"] for x in data_auto_gpu], label="Auto. (GPU), encode", color="tab:red", linestyle="-", alpha=0.75)
-ax.plot(DIMS, [x["encode_time"] for x in data_auto], label="Auto. (CPU), encode", color="tab:pink", linestyle="-", alpha=0.75)
+def plot_line_with_errorbars(key, data, label, color, ax, linestyle="-"):
+    conf_intervals = [
+        st.t.interval(
+            alpha=0.95, df=len(x[key]) - 1,
+            loc=np.average(x[key]), scale=st.sem(x[key])
+        )
+        for x in data
+    ]
+    conf_intervals = [(x[1] - x[0]) / 2 for x in conf_intervals]
+    ax.errorbar(
+        DIMS,
+        [np.average(x[key])
+        for x in data],
+        yerr=conf_intervals,
+        fmt="none",
+        ecolor=color, elinewidth=1.5,
+        alpha=0.2,
+        capsize=4
+    )
+    ax.plot(DIMS, [np.average(x[key]) for x in data], label=label, color=color, linestyle=linestyle, alpha=0.75)
 
+plot_line_with_errorbars("encode_time", data_pca_scikit, label="PCA (scikit)", color="tab:green", ax=ax1)
+plot_line_with_errorbars("encode_time", data_pca_torch_gpu, label="PCA (Torch, GPU)", color="tab:blue", ax=ax1)
+plot_line_with_errorbars("encode_time", data_pca_torch, label="PCA (Torch, CPU)", color="tab:cyan", ax=ax1)
+plot_line_with_errorbars("encode_time", data_auto_gpu, label="Auto. (GPU)", color="tab:red", ax=ax1)
+plot_line_with_errorbars("encode_time", data_auto, label="Auto. (CPU)", color="tab:pink", ax=ax1)
+ax1.set_ylabel("Time (s)")
+# ax1.xaxis.set_visible(False)
+ax1.set_xlabel("Dimension")
+ax1.set_xticks(DISPLAY_DIMS)
+ax1.set_xticklabels(DISPLAY_DIMS)
+ax1.set_title("Encode Time")
 
-ax.plot(DIMS, [x["train_time"] for x in data_pca_scikit], label="PCA (scikit), train", color="tab:green", linestyle=":")
-ax.plot(DIMS, [x["train_time"] for x in data_pca_torch_gpu], label="PCA (Torch, GPU), train", color="tab:blue", linestyle=":")
-ax.plot(DIMS, [x["train_time"] for x in data_pca_torch], label="PCA (Torch, CPU), train", color="tab:cyan", linestyle=":")
-ax.plot(DIMS, [x["train_time"] for x in data_auto_gpu], label="Auto. (GPU), train", color="tab:red", linestyle=":")
-ax.plot(DIMS, [x["train_time"] for x in data_auto], label="Auto. (CPU), train", color="tab:pink", linestyle=":")
+# ax1 = plt.subplot(2, 1, 1)
+# ax2 = plt.subplot(2, 1, 2)
 
-
-ax.set_xticks(DISPLAY_DIMS)
-ax.set_xticklabels(DISPLAY_DIMS)
-ax.set_ylabel("Time (s)")
-ax.set_xlabel("Dimension")
-# ax.set_ylim(0.015, 0.66)
-
-h1, l1 = ax.get_legend_handles_labels()
-
-# plt.title(["No pre-processing", "Normalized", "Centered", "Centered, Normalized"][args.key])
+plt.tight_layout(h_pad=0, rect=(0,0,0.76,1))
+h1, l1 = ax1.get_legend_handles_labels()
 plt.legend(
-    h1, l1,
+    # [extra]+list(h1),
+    # ["$\\textbf{Encode}$"]+list(l1),
     loc="upper left",
-    
     bbox_to_anchor=(1, 1),
     ncol=1,
     columnspacing=1.4,
 )
-plt.tight_layout()
-plt.savefig("figures/model_speed.pdf")
+
+plt.savefig("figures/model_speed_encode.pdf")
+plt.show()
+
+plt.figure(figsize=(7, 5))
+ax2 = plt.gca()
+
+plot_line_with_errorbars("train_time", data_pca_scikit, label="PCA (scikit)", color="tab:green", ax=ax2, linestyle="--")
+plot_line_with_errorbars("train_time", data_pca_torch_gpu, label="PCA (Torch, GPU)", color="tab:blue", ax=ax2, linestyle="--")
+plot_line_with_errorbars("train_time", data_pca_torch, label="PCA (Torch, CPU)", color="tab:cyan", ax=ax2, linestyle="--")
+plot_line_with_errorbars("train_time", data_auto_gpu, label="Auto. (GPU)", color="tab:red", ax=ax2, linestyle="--")
+plot_line_with_errorbars("train_time", data_auto, label="Auto. (CPU)", color="tab:pink", ax=ax2, linestyle="--")
+
+ax2.set_ylabel("Time (s)")
+ax2.set_xlabel("Dimension")
+ax2.set_xticks(DISPLAY_DIMS)
+ax2.set_xticklabels(DISPLAY_DIMS)
+ax2.set_title("Train Time")
+
+h2, l2 = ax2.get_legend_handles_labels()
+
+plt.tight_layout(h_pad=0, rect=(0,0,0.76,1))
+
+
+# plt.title(["No pre-processing", "Normalized", "Centered", "Centered, Normalized"][args.key])
+plt.legend(
+    # [extra]+list(h2),
+    # ["$\\textbf{Train}$"]+list(l2),
+    loc="upper left",
+    bbox_to_anchor=(1, 1),
+    ncol=1,
+    columnspacing=1.4,
+)
+# ax.set_ylim(0.015, 0.66)
+
+plt.savefig("figures/model_speed_train.pdf")
 plt.show()
